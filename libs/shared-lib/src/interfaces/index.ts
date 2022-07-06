524302789eee592f44d3b701fde3f43d941af42f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-07-06 20:39:31 +0100

    extracted StatChartData interface from Statistics interface

--- libs/shared-lib/src/interfaces/index.ts
+++ libs/shared-lib/src/interfaces/index.ts
@@ -3,16 +3,18 @@ export interface Data {
   y: number;
 }
 
+export interface StatChartData {
+  m5: Data[];
+  m15: Data[];
+  m30: Data[];
+  h1: Data[];
+  h4: Data[];
+  d1: Data[];
+}
+
 export interface Statistics {
   original: number;
   replay: number;
   retweet: number;
-  chart: {
-    m5: Data[];
-    m15: Data[];
-    m30: Data[];
-    h1: Data[];
-    h4: Data[];
-    d1: Data[];
-  };
+  chart: StatChartData;
 }
