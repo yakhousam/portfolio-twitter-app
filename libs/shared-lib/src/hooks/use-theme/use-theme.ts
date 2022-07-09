b16653324d5069745b8ff7fe77f452edbcf7575d
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-07-09 20:40:47 +0100

    fixed tweets embed not updating theme

--- libs/shared-lib/src/hooks/use-theme/use-theme.ts
+++ libs/shared-lib/src/hooks/use-theme/use-theme.ts
@@ -7,20 +7,36 @@ export function useTheme(
 ): [Theme, React.Dispatch<React.SetStateAction<Theme>>] {
   const [theme, setTheme] = useState<Theme>(() => {
     if (themeProp) {
-      document.body.dataset.theme = themeProp;
+      document.body.dataset['theme'] = themeProp;
       return themeProp;
     }
-    const theme = document.body.dataset.theme as Theme;
+    const theme = document.body.dataset['theme'] as Theme;
     if (theme) {
       return theme;
     }
-    document.body.dataset.theme = 'light';
+    document.body.dataset['theme'] = 'light';
     return 'light';
   });
   useEffect(() => {
     if (theme !== undefined) {
-      document.body.dataset.theme = theme;
+      document.body.dataset['theme'] = theme;
+      toogleTwitterEmbedTheme(theme);
     }
   }, [theme]);
   return [theme, setTheme];
+}
+
+function toogleTwitterEmbedTheme(theme: Theme) {
+  const tweets = document.querySelectorAll('[data-tweet-id]');
+  const oldTheme = theme === 'dark' ? 'light' : 'dark';
+
+  tweets.forEach(function (tweet) {
+    const src = tweet.getAttribute('src');
+    if (src) {
+      tweet.setAttribute(
+        'src',
+        src.replace('theme=' + oldTheme, 'theme=' + theme)
+      );
+    }
+  });
 }
