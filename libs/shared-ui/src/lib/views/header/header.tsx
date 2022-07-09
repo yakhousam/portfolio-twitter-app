b9c27f6a1890366de5a5c15fbee15d9ea5ada9ee
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-07-09 10:43:58 +0100

    added handleToggleTheme to header

--- libs/shared-ui/src/lib/views/header/header.tsx
+++ libs/shared-ui/src/lib/views/header/header.tsx
@@ -1,16 +1,23 @@
 import styles from './header.module.css';
 import { MdOutlineDarkMode, MdOutlineAccountCircle } from 'react-icons/md';
+import { useTheme } from '@yak-twitter-app/shared-lib';
 
 /* eslint-disable-next-line */
 export interface HeaderProps {}
 
 export function Header(props: HeaderProps) {
+  const [theme, setTheme] = useTheme();
+  const handleToggletheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
   return (
     <header className={styles['header']}>
       <div className={styles['container']}>
         <h1 className={styles['title']}>twitter hashtag analyzer</h1>
         <div className={styles['icons-wrapper']}>
-          <MdOutlineDarkMode className={styles['icon']} />
+          <MdOutlineDarkMode
+            aria-label="toggle theme"
+            className={styles['icon']}
+            onClick={handleToggletheme}
+          />
           <MdOutlineAccountCircle className={styles['icon']} />
         </div>
       </div>
