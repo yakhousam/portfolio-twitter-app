6023b47fac1c37722aeb3c1027dcbe50df375ed0
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-03 11:48:40 +0100

    added @testing-library/jest-dom

--- libs/shared-ui/jest.config.ts
+++ libs/shared-ui/jest.config.ts
@@ -7,4 +7,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/shared-ui',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
 };
