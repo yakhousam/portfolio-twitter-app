d658118026fab72dfd0f2b461174794eb5a6f72a
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-05 19:05:58 +0100

    added chart button interaction test

--- libs/shared-ui/src/lib/components/btn-chart/btn-chart.stories.tsx
+++ libs/shared-ui/src/lib/components/btn-chart/btn-chart.stories.tsx
@@ -1,5 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { screen, userEvent } from '@storybook/testing-library';
+import { jest, expect } from '@storybook/jest';
+
 import { BtnChart } from './btn-chart';
+import { TypeOrArrayOf } from 'twitter-api-v2/dist/types/shared.types';
 
 export default {
   component: BtnChart,
@@ -14,12 +18,20 @@ const Template: ComponentStory<typeof BtnChart> = (args) => (
 );
 
 export const Default = Template.bind({});
+
 Default.args = {
   caption: 'h1',
+  handleClick: jest.fn(),
 };
 
 export const Active = Template.bind({});
 Active.args = {
   ...Default.args,
   active: true,
+};
+
+Default.play = async () => {
+  const button = screen.getByRole('button');
+  await userEvent.click(button);
+  expect(Default.args?.handleClick).toHaveBeenCalled();
 };
