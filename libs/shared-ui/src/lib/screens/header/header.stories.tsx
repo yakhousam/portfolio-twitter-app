8d3aafb0a9b876664e3094dbde3158ea73fcad7a
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-08 21:10:40 +0100

    added header story

--- libs/shared-ui/src/lib/screens/header/header.stories.tsx
+++ libs/shared-ui/src/lib/screens/header/header.stories.tsx
@@ -1,6 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Header } from './header';
 
+import { screen, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
 export default {
   component: Header,
   title: 'screens/Header',
@@ -9,3 +12,14 @@ export default {
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
 export const Default = Template.bind({});
+
+Default.play = async () => {
+  const button = screen.getByLabelText('toggle theme', { selector: 'button' });
+  const theme = document.body.dataset['theme'];
+
+  await userEvent.click(button);
+  expect(document.body.dataset['theme']).not.toBe(theme);
+
+  await userEvent.click(button);
+  expect(document.body.dataset['theme']).toBe(theme);
+};
