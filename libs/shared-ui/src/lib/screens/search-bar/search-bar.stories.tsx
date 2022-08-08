706d6d129089e23cb3ff5cb729c87ff36ed900e8
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-08 21:11:22 +0100

    added search bar interaction tests

--- libs/shared-ui/src/lib/screens/search-bar/search-bar.stories.tsx
+++ libs/shared-ui/src/lib/screens/search-bar/search-bar.stories.tsx
@@ -1,5 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { SearchBar } from './search-bar';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import {
+  formatDateYYYMMDD,
+  getDefaultEndDate,
+  getDefaultStartDate,
+} from '@yak-twitter-app/shared-lib';
 
 export default {
   component: SearchBar,
@@ -17,8 +24,69 @@ export const Default = Template.bind({});
 Default.args = {
   isFetching: false,
 };
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const user = userEvent;
+
+  const startDateInput = canvas.getByLabelText(/start/i);
+  const endDateInput = canvas.getByLabelText(/end/i);
+
+  const startDateMax = new Date(getDefaultEndDate());
+  startDateMax.setDate(startDateMax.getDate() - 1);
+  const endDateMin = new Date(getDefaultStartDate());
+  endDateMin.setDate(endDateMin.getDate() + 1);
+
+  expect(startDateInput).toHaveAttribute('value', getDefaultStartDate());
+  expect(startDateInput).toHaveAttribute('min', getDefaultStartDate());
+  expect(startDateInput).toHaveAttribute(
+    'max',
+    formatDateYYYMMDD(startDateMax)
+  );
+
+  expect(endDateInput).toHaveAttribute('value', getDefaultEndDate());
+  expect(endDateInput).toHaveAttribute('min', formatDateYYYMMDD(endDateMin));
+  expect(endDateInput).toHaveAttribute('max', getDefaultEndDate());
+
+  let date = new Date(getDefaultEndDate());
+  date.setDate(date.getDate() + Math.floor(Math.random() * 100) + 1);
+  await user.clear(startDateInput);
+  await user.type(startDateInput, formatDateYYYMMDD(date));
+  expect(startDateInput).toHaveAttribute(
+    'value',
+    formatDateYYYMMDD(startDateMax)
+  );
+  date = new Date(getDefaultStartDate());
+  date.setDate(date.getDate() - Math.floor(Math.random() * 100) + 1);
+  await user.clear(startDateInput);
+  await user.type(startDateInput, formatDateYYYMMDD(date));
+  expect(startDateInput).toHaveAttribute('value', getDefaultStartDate());
+
+  date = new Date(getDefaultStartDate());
+  date.setDate(date.getDate() - Math.floor(Math.random() * 100) + 1);
+  await user.clear(endDateInput);
+  await user.type(endDateInput, formatDateYYYMMDD(date));
+  expect(endDateInput).toHaveAttribute('value', formatDateYYYMMDD(endDateMin));
+  date = new Date(getDefaultEndDate());
+  date.setDate(date.getDate() + Math.floor(Math.random() * 100) + 1);
+  await user.clear(endDateInput);
+  await user.type(endDateInput, formatDateYYYMMDD(date));
+  expect(endDateInput).toHaveAttribute('value', getDefaultEndDate());
+};
 
 export const Searching = Template.bind({});
 Searching.args = {
   isFetching: true,
+};
+
+export const ErrorSubmittingEmptyHashtag = Template.bind({});
+ErrorSubmittingEmptyHashtag.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const user = userEvent;
+
+  const searchInput = canvas.getByRole('searchbox');
+  const searchButton = canvas.getByLabelText('search');
+
+  await user.clear(searchInput);
+  await user.click(searchButton);
+  expect(canvas.getByText(/error/i)).toBeInTheDocument();
 };
--- libs/shared-ui/src/lib/screens/search-bar/search-bar.tsx
+++ libs/shared-ui/src/lib/screens/search-bar/search-bar.tsx
@@ -5,7 +5,7 @@ import {
   isDateValid,
   SearchForm,
 } from '@yak-twitter-app/shared-lib';
-import { ChangeEvent, memo, useReducer } from 'react';
+import { ChangeEvent, useReducer } from 'react';
 import { MdSearch } from 'react-icons/md';
 import BtnSearch from '../../components/btn-search/btn-search';
 import InputDate from '../../components/input-date/input-date';
@@ -16,46 +16,69 @@ import styles from './search-bar.module.css';
 
 export type ActionType =
   | { type: 'set_hashtag'; value: string }
+  | { type: 'set_hashtag_error' }
   | { type: 'set_startDate'; value: string }
   | { type: 'set_endDate'; value: string };
 
-const intialState = {
-  hashtag: 'node',
+const intialState: SearchForm = {
+  hashtag: '',
   startDate: getDefaultStartDate(),
   endDate: getDefaultEndDate(),
+  errors: {
+    hashtag: false,
+  },
 };
 
 function reducer(state: SearchForm, action: ActionType): SearchForm {
-  const { type, value } = action;
+  const { type } = action;
   const defaultStartDate = getDefaultStartDate();
   const defaultEndDate = getDefaultEndDate();
   switch (type) {
     case 'set_hashtag': {
-      return { ...state, hashtag: value };
+      return {
+        ...state,
+        hashtag: action.value,
+        errors: { ...state.errors, hashtag: false },
+      };
     }
     case 'set_startDate': {
-      if (!isDateValid(value)) {
+      if (!isDateValid(action.value)) {
         return { ...state };
       }
-      if (new Date(value).getTime() < new Date(defaultStartDate).getTime()) {
+      if (
+        new Date(action.value).getTime() < new Date(defaultStartDate).getTime()
+      ) {
         return { ...state, startDate: defaultStartDate };
       }
-      if (new Date(value).getTime() > new Date(defaultEndDate).getTime()) {
-        return { ...state, startDate: defaultEndDate };
+      if (
+        new Date(action.value).getTime() > new Date(defaultEndDate).getTime()
+      ) {
+        const d = new Date(defaultEndDate);
+        d.setDate(d.getDate() - 1);
+        return { ...state, startDate: formatDateYYYMMDD(d) };
       }
-      return { ...state, startDate: value };
+      return { ...state, startDate: action.value };
     }
     case 'set_endDate': {
-      if (!isDateValid(value)) {
+      if (!isDateValid(action.value)) {
         return { ...state };
       }
-      if (new Date(value).getTime() < new Date(defaultStartDate).getTime()) {
-        return { ...state, endDate: defaultStartDate };
+      if (
+        new Date(action.value).getTime() < new Date(defaultStartDate).getTime()
+      ) {
+        const d = new Date(defaultStartDate);
+        d.setDate(d.getDate() + 1);
+        return { ...state, endDate: formatDateYYYMMDD(d) };
       }
-      if (new Date(value).getTime() > new Date(defaultEndDate).getTime()) {
+      if (
+        new Date(action.value).getTime() > new Date(defaultEndDate).getTime()
+      ) {
         return { ...state, endDate: defaultEndDate };
       }
-      return { ...state, endDate: value };
+      return { ...state, endDate: action.value };
+    }
+    case 'set_hashtag_error': {
+      return { ...state, errors: { ...state.errors, hashtag: true } };
     }
     default:
       throw new Error(`action type "${type}" doesn't exist`);
@@ -63,72 +86,72 @@ function reducer(state: SearchForm, action: ActionType): SearchForm {
 }
 
 export interface SearchBarProps {
-  onSubmit: (data: SearchForm) => void;
+  onSubmit: (data: Omit<SearchForm, 'errors'>) => void;
   isFetching: boolean;
 }
 
-export const SearchBar = memo(
-  function SearchBar({ isFetching, onSubmit }: SearchBarProps) {
-    const [{ hashtag, startDate, endDate }, dispatch] = useReducer(
-      reducer,
-      intialState
-    );
+export function SearchBar({ isFetching, onSubmit }: SearchBarProps) {
+  const [{ hashtag, startDate, endDate, errors }, dispatch] = useReducer(
+    reducer,
+    intialState
+  );
 
-    const maxStartDate = new Date(endDate);
-    maxStartDate.setDate(maxStartDate.getDate() - 1);
-    const minEndDate = new Date(startDate);
-    minEndDate.setDate(minEndDate.getDate() + 1);
+  const maxStartDate = new Date(endDate);
+  maxStartDate.setDate(maxStartDate.getDate() - 1);
+  const minEndDate = new Date(startDate);
+  minEndDate.setDate(minEndDate.getDate() + 1);
 
-    return (
-      <form
-        className={styles['container']}
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit({ hashtag, startDate, endDate });
-        }}
-      >
-        <div className={styles['wrapper-search']}>
-          <InputSearch
-            name="hashtag"
-            value={hashtag}
-            handleChange={(e: ChangeEvent<HTMLInputElement>) =>
-              dispatch({ type: 'set_hashtag', value: e.target.value })
-            }
-          />
-          <BtnSearch>
-            {isFetching ? 'CANCEL' : <MdSearch className={styles['icon']} />}
-          </BtnSearch>
-        </div>
-        <div className={styles['wrapper-options']}>
-          <InputDate
-            label="start date"
-            value={startDate}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              dispatch({ type: 'set_startDate', value: e.target.value })
-            }
-            {...{
-              name: 'startDate',
-              min: getDefaultStartDate(),
-              max: formatDateYYYMMDD(maxStartDate),
-            }}
-          />
-          <InputDate
-            label="end date"
-            value={endDate}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              dispatch({ type: 'set_endDate', value: e.target.value })
-            }
-            {...{
-              name: 'endDate',
-              min: formatDateYYYMMDD(minEndDate),
-              max: getDefaultEndDate(),
-            }}
-          />
-        </div>
-      </form>
-    );
-  },
-  (prevprops, nextprops) => prevprops.isFetching === nextprops.isFetching
-);
+  return (
+    <form
+      className={styles['container']}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (!hashtag) {
+          return dispatch({ type: 'set_hashtag_error' });
+        }
+        onSubmit({ hashtag, startDate, endDate });
+      }}
+    >
+      <div className={styles['wrapper-search']}>
+        <InputSearch
+          value={hashtag}
+          error={errors.hashtag}
+          handleChange={(e: ChangeEvent<HTMLInputElement>) =>
+            dispatch({ type: 'set_hashtag', value: e.target.value })
+          }
+        />
+        <BtnSearch>
+          {isFetching ? 'CANCEL' : <MdSearch className={styles['icon']} />}
+        </BtnSearch>
+      </div>
+      <div className={styles['wrapper-options']}>
+        <InputDate
+          label="start date"
+          value={startDate}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            dispatch({ type: 'set_startDate', value: e.target.value })
+          }
+          {...{
+            name: 'startDate',
+            min: getDefaultStartDate(),
+            max: formatDateYYYMMDD(maxStartDate),
+          }}
+        />
+        <InputDate
+          label="end date"
+          value={endDate}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            dispatch({ type: 'set_endDate', value: e.target.value })
+          }
+          {...{
+            name: 'endDate',
+            min: formatDateYYYMMDD(minEndDate),
+            max: getDefaultEndDate(),
+          }}
+        />
+      </div>
+    </form>
+  );
+}
 
 export default SearchBar;
