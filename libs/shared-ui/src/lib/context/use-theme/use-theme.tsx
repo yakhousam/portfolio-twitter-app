cf7bd091b6d82cc6320f1c3f391cbea09c10c61d
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-14 13:58:54 +0100

    rf useTheme

--- libs/shared-ui/src/lib/context/use-theme/use-theme.tsx
+++ libs/shared-ui/src/lib/context/use-theme/use-theme.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 export type Theme = 'dark' | 'light';
 
@@ -7,14 +7,44 @@ const ThemContext = createContext<
 >(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (document.body.dataset['theme'] as Theme) || 'light'
-  );
+  const [theme, setTheme] = useState<Theme>(() => {
+    let theme: Theme;
+    const root = document.querySelector(':root') as HTMLHtmlElement;
+    const localStorageTheme = localStorage.getItem('theme');
+    const osTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (localStorageTheme) {
+      theme = localStorageTheme as Theme;
+      root.className = theme;
+    } else if (osTheme) {
+      theme = 'dark';
+    } else {
+      theme = 'light';
+    }
+    return theme;
+  });
+
+  useEffect(() => {
+    const listener = function (e: MediaQueryListEvent) {
+      const theme = e.matches ? 'dark' : 'light';
+      const root = document.querySelector(':root') as HTMLHtmlElement;
+      root.className = theme;
+      setTheme(theme);
+    };
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', listener);
+    return () =>
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener('change', listener);
+  }, []);
 
   const toggleTheme = () => {
-    const t = theme ? (theme === 'dark' ? 'light' : 'dark') : 'light';
-    document.body.dataset['theme'] = t;
-    setTheme(t);
+    const toggledTheme = theme === 'dark' ? 'light' : 'dark';
+    const root = document.querySelector(':root') as HTMLHtmlElement;
+    root.className = toggledTheme;
+    setTheme(toggledTheme);
+    localStorage.setItem('theme', toggledTheme);
   };
   return (
     <ThemContext.Provider value={{ theme, toggleTheme }}>
