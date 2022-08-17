25758df4cf3a2dadcb1a82559598f86310ce089f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-17 18:53:51 +0100

    added timer cmp story

--- libs/shared-ui/src/lib/components/timer/timer.stories.tsx
+++ libs/shared-ui/src/lib/components/timer/timer.stories.tsx
@@ -1,5 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { Timer } from './timer';
+import { sleep } from '@yak-twitter-app/shared-lib';
 
 export default {
   component: Timer,
@@ -14,6 +17,15 @@ const Template: ComponentStory<typeof Timer> = (args) => <Timer {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  title: 'timer',
-  seconds: 10,
+  label: 'timer',
+  seconds: 3,
+};
+
+Default.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+  expect(canvas.getByLabelText(args.label)).toBeInTheDocument();
+  const regexp = new RegExp(String(args.seconds), 'i');
+  expect(await canvas.findByText(regexp)).toBeInTheDocument();
+  await sleep((args.seconds + 1) * 1000);
+  await expect(args.onTimerEnd).toHaveBeenCalled();
 };
