35654e619a37b8892c2aa98ee2ec016486df966b
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-17 18:53:02 +0100

    removed theme tweet wrapper

--- libs/shared-ui/src/lib/screens/twitter-timeline-embed-list/twitter-timeline-embed-list.tsx
+++ libs/shared-ui/src/lib/screens/twitter-timeline-embed-list/twitter-timeline-embed-list.tsx
@@ -1,20 +1,18 @@
 import TwitterTimelineEmbed from '../../components/twitter-timeline-embed/twitter-timeline-embed';
 import { useAppData } from '../../context/use-app-data/use-app-data';
-import { useTheme } from '../../context/use-theme/use-theme';
 import styles from './twitter-timeline-embed-list.module.css';
 
 export function TwitterTimelineEmbedList() {
   const {
     state: { mostFollowedAccountIds },
   } = useAppData();
-  const { theme } = useTheme();
-
+  console.log({ mostFollowedAccountIds });
   return (
     <section className={styles['container']}>
       <h2 className={styles['title']}>most followed accounts</h2>
       <div className={styles['tweets-container']}>
         {mostFollowedAccountIds.map((id) => (
-          <TwitterTimelineEmbed key={id} userId={id} theme={theme} />
+          <TwitterTimelineEmbed key={id} userId={id} />
         ))}
       </div>
     </section>
--- libs/shared-ui/src/lib/screens/twitter-tweet-embed-list/twitter-tweet-embed-list.tsx
+++ libs/shared-ui/src/lib/screens/twitter-tweet-embed-list/twitter-tweet-embed-list.tsx
@@ -1,21 +1,18 @@
 import TwitterTweetEmbed from '../../components/twitter-tweet-embed/twitter-tweet-embed';
 import { useAppData } from '../../context/use-app-data/use-app-data';
-import { useTheme } from '../../context/use-theme/use-theme';
 import styles from './twitter-tweet-embed-list.module.css';
 
 export function TwitterTweetEmbedList() {
   const {
     state: { mostEngagedTweetsIds },
   } = useAppData();
-
-  const { theme } = useTheme();
-
+  console.log({ mostEngagedTweetsIds });
   return (
     <section className={styles['container']}>
       <h2 className={styles['title']}>most engaged tweets</h2>
       <div className={styles['tweets-container']}>
         {mostEngagedTweetsIds.map((id) => (
-          <TwitterTweetEmbed key={id} tweetId={id} theme={theme} />
+          <TwitterTweetEmbed key={id} tweetId={id} />
         ))}
       </div>
     </section>
