d4c8dca38e51912e99836e44ddd10eeda3beef74
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-28 22:39:23 +0100

    fixed counter up test

--- libs/web/ui/components/counter-up/src/lib/counter-up.stories.tsx
+++ libs/web/ui/components/counter-up/src/lib/counter-up.stories.tsx
@@ -16,7 +16,7 @@ const Template: ComponentStory<typeof CounterUp> = (args) => (
 export const Default = Template.bind({});
 
 Default.args = {
-  title: 'counter up',
+  label: 'counter up',
   from: 0,
   to: 50,
   spead: 3,
@@ -24,8 +24,17 @@ Default.args = {
 
 Default.play = async ({ args, canvasElement }) => {
   const canvas = within(canvasElement);
-  const counter = canvas.getByLabelText(args.title);
-  expect(counter).toHaveTextContent(String(args.from));
+  await expect(canvas.getByLabelText(args.label)).toBeInTheDocument();
+
+  await expect(
+    canvas.getByTestId(`${args.label}-aria-value`)
+  ).toHaveTextContent(args.to.toString());
+
+  await expect(canvas.getByTestId(`${args.label}-value`)).toHaveTextContent(
+    args.from.toString()
+  );
   await sleep((20 * args.to) / (args.spead || 1));
-  expect(counter).toHaveTextContent(String(args.to));
+  await expect(canvas.getByTestId(`${args.label}-value`)).toHaveTextContent(
+    args.to.toString()
+  );
 };
