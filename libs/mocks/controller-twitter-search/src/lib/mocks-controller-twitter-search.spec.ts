2118efab43cd2cfe10686ae05ed42ea854a07802
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-08-29 18:48:47 +0100

    remove mock-controller

--- libs/mocks/controller-twitter-search/src/lib/mocks-controller-twitter-search.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mocksControllerTwitterSearch } from './mocks-controller-twitter-search';
-
-describe('mocksControllerTwitterSearch', () => {
-  it('should work', () => {
-    expect(mocksControllerTwitterSearch()).toEqual(
-      'mocks-controller-twitter-search'
-    );
-  });
-});
