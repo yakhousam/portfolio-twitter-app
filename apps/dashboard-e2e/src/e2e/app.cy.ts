4be317909817942d1369b13e22ce60953c8b1908
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-09-12 19:19:01 +0100

    migrate to cypress 10

--- apps/dashboard-e2e/src/e2e/app.cy.ts
+++ apps/dashboard-e2e/src/e2e/app.cy.ts
@@ -8,6 +8,6 @@ describe('dashboard', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome dashboard');
+    getGreeting().contains('twitter hashtag analyzer');
   });
 });
