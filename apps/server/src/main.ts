d310d36c52ddca0b713127742f7eb36b30afc9ac
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-09-16 22:31:32 +0100

    move server-app code to apps server main

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,7 +1,79 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import '@yak-twitter-app/server/db';
-import { app } from '@yak-twitter-app/server/app';
+import * as path from 'path';
+import * as express from 'express';
+import * as compression from 'compression';
+import * as passport from 'passport';
+import '@yak-twitter-app/server/passport';
+import * as MongoDBStore from 'connect-mongodb-session';
+import * as session from 'express-session';
+import { searchHashtagRoute } from '@yak-twitter-app/server-routes-search-hashtag';
+import { authRoute } from '@yak-twitter-app/server/routes/auth';
+import { errorMiddleware } from '@yak-twitter-app/server-middlewares-error';
+import { IUser } from '@yak-twitter-app/types';
+
+const app = express();
+
+app.use(compression());
+
+const mongoDbStore = MongoDBStore(session);
+
+const store = new mongoDbStore({
+  uri: process.env.DB_URI || 'mongodb://localhost/twitterapp',
+  collection: 'session',
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'this is a Secret',
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, //one day
+    },
+    resave: false,
+    saveUninitialized: true,
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(authRoute);
+
+function isLoggedInMiddleware(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  if (!req.isAuthenticated()) {
+    return res.status(403).end();
+  }
+  next();
+}
+
+app.use('/api/search/hashtag', isLoggedInMiddleware, searchHashtagRoute);
+
+app.use(express.static(path.resolve(__dirname, 'assets', 'public')));
+app.get('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/dashboard');
+  }
+  res.sendFile(path.resolve(__dirname, 'assets', 'index.html'));
+});
+
+app.use(express.static(path.resolve(__dirname, '..', 'dashboard')));
+
+app.get('/dashboard', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  const user = req.user as IUser;
+  res.cookie('user_avatar_url', user.profile?.photos?.[0].value);
+  res.sendFile(path.resolve(__dirname, '..', 'dashboard', 'index.html'));
+});
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3333;
 
