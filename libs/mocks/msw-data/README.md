2dca56173abf0b9f120674f89ffa5702a6220c37
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-09-16 22:28:54 +0100

    add msw mock data

---
+++ libs/mocks/msw-data/README.md
@@ -0,0 +1,7 @@
+# mocks-msw-data
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build mocks-msw-data` to build the library.
---
+++ libs/mocks/msw-data/src/lib/mocks-msw-data.ts
@@ -0,0 +1,207 @@
+import * as data_0 from './data/data_0.json';
+import * as data_1 from './data/data_1.json';
+import * as data_2 from './data/data_2.json';
+import * as data_3 from './data/data_3.json';
+import * as data_4 from './data/data_4.json';
+import * as data_5 from './data/data_5.json';
+import * as data_6 from './data/data_6.json';
+import * as data_7 from './data/data_7.json';
+import * as data_8 from './data/data_8.json';
+import * as data_9 from './data/data_9.json';
+import * as data_10 from './data/data_10.json';
+import * as data_11 from './data/data_11.json';
+import * as data_12 from './data/data_12.json';
+import * as data_13 from './data/data_5.json';
+import * as data_14 from './data/data_14.json';
+import * as data_15 from './data/data_15.json';
+import * as data_16 from './data/data_16.json';
+import * as data_17 from './data/data_17.json';
+import * as data_18 from './data/data_18.json';
+import * as data_19 from './data/data_19.json';
+import * as data_20 from './data/data_20.json';
+import * as data_21 from './data/data_21.json';
+import * as data_22 from './data/data_22.json';
+import * as data_23 from './data/data_23.json';
+import * as data_24 from './data/data_24.json';
+import * as data_25 from './data/data_25.json';
+import * as data_26 from './data/data_26.json';
+import * as data_27 from './data/data_27.json';
+import * as data_28 from './data/data_28.json';
+import * as data_29 from './data/data_29.json';
+import * as data_30 from './data/data_30.json';
+import * as data_31 from './data/data_31.json';
+import * as data_32 from './data/data_32.json';
+import * as data_33 from './data/data_33.json';
+import * as data_34 from './data/data_34.json';
+import * as data_35 from './data/data_35.json';
+import * as data_36 from './data/data_36.json';
+import * as data_37 from './data/data_37.json';
+import * as data_38 from './data/data_38.json';
+import * as data_39 from './data/data_39.json';
+import * as data_40 from './data/data_40.json';
+import * as data_41 from './data/data_41.json';
+import * as data_42 from './data/data_42.json';
+import * as data_43 from './data/data_43.json';
+import * as data_44 from './data/data_44.json';
+import * as data_45 from './data/data_45.json';
+import * as data_46 from './data/data_46.json';
+import * as data_47 from './data/data_47.json';
+import * as data_48 from './data/data_48.json';
+import * as data_49 from './data/data_49.json';
+
+export const dumyData: Record<string, typeof data_49> = {
+  '0': data_0,
+  [data_0.meta.next_token]: data_1,
+  [data_1.meta.next_token]: data_2,
+  [data_2.meta.next_token]: data_3,
+  [data_3.meta.next_token]: data_4,
+  [data_4.meta.next_token]: data_5,
+  [data_5.meta.next_token]: data_6,
+  [data_6.meta.next_token]: data_7,
+  [data_7.meta.next_token]: data_8,
+  [data_8.meta.next_token]: data_9,
+  [data_9.meta.next_token]: data_10,
+  [data_10.meta.next_token]: data_11,
+  [data_11.meta.next_token]: data_12,
+  [data_12.meta.next_token]: data_13,
+  [data_13.meta.next_token]: data_14,
+  [data_14.meta.next_token]: data_15,
+  [data_15.meta.next_token]: data_16,
+  [data_16.meta.next_token]: data_17,
+  [data_17.meta.next_token]: data_18,
+  [data_18.meta.next_token]: data_19,
+  [data_19.meta.next_token]: data_20,
+  [data_20.meta.next_token]: data_21,
+  [data_21.meta.next_token]: data_22,
+  [data_22.meta.next_token]: data_23,
+  [data_23.meta.next_token]: data_24,
+  [data_24.meta.next_token]: data_25,
+  [data_25.meta.next_token]: data_26,
+  [data_26.meta.next_token]: data_27,
+  [data_27.meta.next_token]: data_28,
+  [data_28.meta.next_token]: data_29,
+  [data_29.meta.next_token]: data_30,
+  [data_30.meta.next_token]: data_31,
+  [data_31.meta.next_token]: data_32,
+  [data_32.meta.next_token]: data_33,
+  [data_33.meta.next_token]: data_34,
+  [data_34.meta.next_token]: data_35,
+  [data_35.meta.next_token]: data_36,
+  [data_36.meta.next_token]: data_37,
+  [data_37.meta.next_token]: data_38,
+  [data_38.meta.next_token]: data_39,
+  [data_39.meta.next_token]: data_40,
+  [data_40.meta.next_token]: data_41,
+  [data_41.meta.next_token]: data_42,
+  [data_42.meta.next_token]: data_43,
+  [data_43.meta.next_token]: data_44,
+  [data_44.meta.next_token]: data_45,
+  [data_45.meta.next_token]: data_46,
+  [data_46.meta.next_token]: data_47,
+  [data_47.meta.next_token]: data_48,
+  [data_48.meta.next_token]: data_49,
+};
+
+export const page = {
+  1: '0',
+  2: data_1.meta.next_token,
+  3: data_2.meta.next_token,
+  4: data_3.meta.next_token,
+  5: data_4.meta.next_token,
+  6: data_5.meta.next_token,
+  7: data_6.meta.next_token,
+  8: data_7.meta.next_token,
+  9: data_8.meta.next_token,
+  10: data_9.meta.next_token,
+  11: data_10.meta.next_token,
+  12: data_11.meta.next_token,
+  13: data_12.meta.next_token,
+  14: data_13.meta.next_token,
+  15: data_14.meta.next_token,
+  16: data_15.meta.next_token,
+  17: data_16.meta.next_token,
+  18: data_17.meta.next_token,
+  19: data_18.meta.next_token,
+  20: data_19.meta.next_token,
+  21: data_20.meta.next_token,
+  22: data_21.meta.next_token,
+  23: data_22.meta.next_token,
+  24: data_23.meta.next_token,
+  25: data_24.meta.next_token,
+  26: data_25.meta.next_token,
+  27: data_26.meta.next_token,
+  28: data_27.meta.next_token,
+  29: data_28.meta.next_token,
+  30: data_29.meta.next_token,
+  31: data_30.meta.next_token,
+  32: data_31.meta.next_token,
+  33: data_32.meta.next_token,
+  34: data_33.meta.next_token,
+  35: data_34.meta.next_token,
+  36: data_35.meta.next_token,
+  37: data_36.meta.next_token,
+  38: data_37.meta.next_token,
+  39: data_38.meta.next_token,
+  40: data_39.meta.next_token,
+  41: data_40.meta.next_token,
+  42: data_41.meta.next_token,
+  43: data_42.meta.next_token,
+  44: data_43.meta.next_token,
+  45: data_44.meta.next_token,
+  46: data_45.meta.next_token,
+  47: data_46.meta.next_token,
+  48: data_47.meta.next_token,
+  49: data_48.meta.next_token,
+};
+
+export const pageByToken = {
+  '0': 1,
+  [data_1.meta.next_token]: 2,
+  [data_2.meta.next_token]: 3,
+  [data_3.meta.next_token]: 4,
+  [data_4.meta.next_token]: 5,
+  [data_5.meta.next_token]: 6,
+  [data_6.meta.next_token]: 7,
+  [data_7.meta.next_token]: 8,
+  [data_8.meta.next_token]: 9,
+  [data_9.meta.next_token]: 10,
+  [data_10.meta.next_token]: 11,
+  [data_11.meta.next_token]: 12,
+  [data_12.meta.next_token]: 13,
+  [data_13.meta.next_token]: 14,
+  [data_14.meta.next_token]: 15,
+  [data_15.meta.next_token]: 16,
+  [data_16.meta.next_token]: 17,
+  [data_17.meta.next_token]: 18,
+  [data_18.meta.next_token]: 19,
+  [data_19.meta.next_token]: 20,
+  [data_20.meta.next_token]: 21,
+  [data_21.meta.next_token]: 22,
+  [data_22.meta.next_token]: 23,
+  [data_23.meta.next_token]: 24,
+  [data_24.meta.next_token]: 25,
+  [data_25.meta.next_token]: 26,
+  [data_26.meta.next_token]: 27,
+  [data_27.meta.next_token]: 28,
+  [data_28.meta.next_token]: 29,
+  [data_29.meta.next_token]: 30,
+  [data_30.meta.next_token]: 31,
+  [data_31.meta.next_token]: 32,
+  [data_32.meta.next_token]: 33,
+  [data_33.meta.next_token]: 34,
+  [data_34.meta.next_token]: 35,
+  [data_35.meta.next_token]: 36,
+  [data_36.meta.next_token]: 37,
+  [data_37.meta.next_token]: 38,
+  [data_38.meta.next_token]: 39,
+  [data_39.meta.next_token]: 40,
+  [data_40.meta.next_token]: 41,
+  [data_41.meta.next_token]: 42,
+  [data_42.meta.next_token]: 43,
+  [data_43.meta.next_token]: 44,
+  [data_44.meta.next_token]: 45,
+  [data_45.meta.next_token]: 46,
+  [data_46.meta.next_token]: 47,
+  [data_47.meta.next_token]: 48,
+  [data_48.meta.next_token]: 49,
+};
