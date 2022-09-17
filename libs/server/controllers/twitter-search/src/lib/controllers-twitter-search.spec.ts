d7c0df72300394fd87ce7e00e336bae36244df20
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-09-17 16:30:09 +0100

    fix controller test

--- libs/server/controllers/twitter-search/src/lib/controllers-twitter-search.spec.ts
+++ libs/server/controllers/twitter-search/src/lib/controllers-twitter-search.spec.ts
@@ -1,25 +1,25 @@
+import * as express from 'express';
 import { Server } from 'http';
 import axios from 'axios';
 import { rest } from 'msw';
-import { server as mockTwitterApi } from '@yak-twitter-app/mocks/server';
+import { dumyData, page } from '@yak-twitter-app/mocks/msw-data';
+import { setupServer } from 'msw/node';
+import { searchByHashtag, SearchRequest } from './controllers-twitter-search';
 
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { app } from '@yak-twitter-app/server/app';
-import { dumyData, page } from '@yak-twitter-app/mocks/server';
-import { getTimestamp } from '@yak-twitter-app/utility/date';
+const mockTwitterApi = setupServer();
 
 jest.setTimeout(1000 * 20);
-
-jest.mock('@yak-twitter-app/utility/helpers', () => {
-  return {
-    sleep: jest.fn(() => Promise.resolve()),
-  };
-});
 
 const PORT = 5003;
 describe('testing routes', () => {
   let server: Server;
   beforeAll((done) => {
+    process.env.TWITTER_CONSUMER_KEY = 'my_consumer_key';
+    process.env.TWITTER_CONSUMER_SECRET = 'my_consumer_secret';
+    const app = express();
+    app.use('/api/search/hashtag', (req: SearchRequest, res, next) =>
+      searchByHashtag(req, res, next)
+    );
     server = app.listen(PORT, () => done());
     mockTwitterApi.listen({
       onUnhandledRequest: 'bypass',
@@ -34,7 +34,23 @@ describe('testing routes', () => {
       rest.get(
         'https://api.twitter.com/2/tweets/search/recent',
         (req, res, ctx) => {
-          return res(ctx.json(dumyData[page[5]]));
+          return res(
+            ctx.set(
+              'x-rate-limit-limit',
+              dumyData['0'].headers['x-rate-limit-limit']
+            ),
+            ctx.set(
+              'x-rate-limit-remaining',
+              dumyData['0'].headers['x-rate-limit-remaining']
+            ),
+            ctx.set(
+              'x-rate-limit-reset',
+              dumyData['0'].headers['x-rate-limit-reset']
+            ),
+            ctx.json({
+              ...dumyData['0'].data,
+            })
+          );
         }
       )
     );
@@ -58,7 +74,23 @@ describe('testing routes', () => {
           // the first time we call twitter search recent, next_token is always undefined
           // I have 5 pages, to speed up the test, I start from page 3
           next_token = req.url.searchParams.get('next_token');
-          return res(ctx.json(dumyData[next_token]));
+          return res(
+            ctx.set(
+              'x-rate-limit-limit',
+              dumyData[next_token].headers['x-rate-limit-limit']
+            ),
+            ctx.set(
+              'x-rate-limit-remaining',
+              dumyData[next_token].headers['x-rate-limit-remaining']
+            ),
+            ctx.set(
+              'x-rate-limit-reset',
+              dumyData[next_token].headers['x-rate-limit-reset']
+            ),
+            ctx.json({
+              ...dumyData[next_token].data,
+            })
+          );
         }
       )
     );
