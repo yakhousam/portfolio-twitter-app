b6e904d43649292375eef0018019a3079ce07c27
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-09-22 13:47:27 +0100

    add e2e test

--- apps/dashboard-e2e/src/e2e/app.cy.ts
+++ apps/dashboard-e2e/src/e2e/app.cy.ts
@@ -1,4 +1,14 @@
-import { getGreeting } from '../support/app.po';
+import { dumyData, page } from '@yak-twitter-app/mocks/msw-data';
+import { SearchHashtagReturnData } from '@yak-twitter-app/types';
+import { getTimestamp } from '@yak-twitter-app/utility/date';
+import {
+  getMostEngagedTweets,
+  getRankedAccounts,
+  getTweetsStats,
+} from '@yak-twitter-app/utility/tweets';
+import { TweetV2 } from 'twitter-api-v2';
+
+// import { getGreeting } from '../support/app.po';
 
 describe('dashboard', () => {
   beforeEach(() => cy.visit('/'));
@@ -8,6 +18,27 @@ describe('dashboard', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('twitter hashtag analyzer');
+    // getGreeting().contains('twitter hashtag analyzer');
+
+    cy.findByRole('heading', { level: 1, name: /twitter hashtag analyzer/ });
+  });
+  it.only(`sould fetch the data`, () => {
+    const result = dumyData[page['49']];
+    const response: SearchHashtagReturnData = {
+      ...getTweetsStats(result.data.data as TweetV2[]),
+      rateLimit: {
+        limit: 180,
+        remaining: 179,
+        reset: getTimestamp(60 * 15),
+      },
+      rankedAccounts: getRankedAccounts(result.data.includes.users),
+      mostEngagedTweets: getMostEngagedTweets(result.data.data as TweetV2[]),
+      chartData: result.data.data.map((tweet) => tweet.created_at),
+      nextToken: result.data.meta.next_token,
+    };
+    cy.intercept('/api/search/hashtag/*', response);
+
+    cy.findByRole('searchbox').type('typescript');
+    cy.findByLabelText('search').click();
   });
 });
